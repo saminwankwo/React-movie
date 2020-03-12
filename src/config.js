ba2f5c1b4d6e17163534dc9bd010bc50853fc507
Nwankwo Samuel <nwankwosami@gmail.com>
2020-03-12 17:46:29 +0100

    added api key, exporting movies files

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 // Read more about the API here: https://developers.themoviedb.org/
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+const API_KEY = '2faf3631a6fa14914342240452cbdeda';
 const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&query=`;
