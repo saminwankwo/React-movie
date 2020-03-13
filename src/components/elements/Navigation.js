3599f0e9a41f7c909bf57b2119acb6c55afbc288
Nwankwo Samuel <nwankwosami@gmail.com>
2020-03-13 03:47:12 +0100

    styled and added movie components

--- src/components/elements/Navigation.js
+++ src/components/elements/Navigation.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from '@reach/router';
+import PropTypes from 'prop-types';
 
-const Navigation = () => <div>Navigation</div>;
+import { StyledNavigation } from '../styles/StyledNavigation';
+
+const Navigation = ({ movie }) => (
+  <StyledNavigation>
+    <div className="navigation-content">
+      <Link to="/">
+        <p>Home</p>
+      </Link>
+      <p>|</p>
+      <p>{movie}</p>
+    </div>
+  </StyledNavigation>
+);
+
+Navigation.propTypes = {
+  movie: PropTypes.string,
+}
 
 export default Navigation;
